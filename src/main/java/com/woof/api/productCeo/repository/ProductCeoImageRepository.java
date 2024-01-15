22f0dfd70adced0b39ba13e48b36224bb114dad4
LCH-97 <leechanghoon12@gmail.com>
2024-01-15 20:22:31 +0900

    [Feat] 에러코드 예외처리 최초 커밋2

--- src/main/java/com/woof/api/productCeo/repository/ProductCeoImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.woof.api.productCeo.repository;
-
-import com.woof.api.productCeo.model.ProductCeoImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductCeoImageRepository extends JpaRepository<ProductCeoImage, Long> {
-}
