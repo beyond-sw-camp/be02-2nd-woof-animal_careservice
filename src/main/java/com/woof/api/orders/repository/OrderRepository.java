2d7829a48c40e6380f5e0a53d8007ea76463dbe3
kangmoonhye <ansgp0323@naver.com>
2024-01-24 17:25:18 +0900

    [Feat] orders 코드 수정

--- src/main/java/com/woof/api/orders/repository/OrderRepository.java
+++ src/main/java/com/woof/api/orders/repository/OrderRepository.java
@@ -6,12 +6,16 @@ import com.woof.api.orders.model.Orders;
 import com.woof.api.orders.model.dto.OrderDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Orders, Long> {
     public Optional<Orders> findById(Long idx);
 
     public Optional<OrderDto> findByPhoneNumber(Long phoneNumber);
+
+    List<OrderDto> list = new ArrayList<>();
 }
 
 
